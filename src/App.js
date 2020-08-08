5151cbcbb02609ece6623cd327ffbc1296810de3
CraigStueber <craig@kurbmedia.com>
2020-08-08 16:14:15 -0400

    WOrking on stuff

--- src/App.js
+++ src/App.js
@@ -41,11 +41,21 @@ function App() {
 return (
 
     <div>
-      <h1>Hello World</h1>
+      <h1>List of shit</h1>
+      <hr />
       {
         courses.map(function(course) {
-          return <div>
-            {course.title}
+          return <div
+            key={course.id}>
+            <span>
+              <a href={course.url}><h4>{course.title}</h4></a>
+            </span>
+            <span>| by <strong>{course.author}</strong></span>
+            <span>| Video Hours: {course.hours_video}</span>
+            <span>| Number of lectures: {course.number_of_lectures}</span>
+            <span>| Rating: {course.rating}</span>
+            <br/>
+            <br/>
           </div>
         })
       }
