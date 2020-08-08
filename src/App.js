fb82a0abb6dbfd7be01190ed9ecc636a7e971aba
CraigStueber <craig@kurbmedia.com>
2020-08-08 15:55:42 -0400

    array play

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React from 'react';
 
+const courses = [
+  {
+    id:1,
+    title: "Bob likes things",
+    author: "Maximilian Schwarzmuller",
+    hours_video: 40.5,
+    number_of_lectures: 490,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/react-complete-guide/"
+
+  },
+  {
+    id:2,
+    title: "The fuck",
+    author: "Maximilian Schwarzmuller",
+    hours_video: 40.5,
+    number_of_lectures: 490,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/react-complete-guide/"
+
+  },
+  {
+    id:3,
+    title: "I like to eat ass",
+    author: "Maximilian Schwarzmuller",
+    hours_video: 40.5,
+    number_of_lectures: 490,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/react-complete-guide/"
+
+  }
+
+
+
+];
+
 
 function App() {
-  const greeting = {
-    greeting_one: 'Hello',
-    greeting_two: 'World'
-  };
-  
-  
-  return (
+return (
 
     <div>
-      <h1>{greeting.greeting_one} {greeting.greeting_two}</h1>
+      <h1>Hello World</h1>
+      {
+        courses.map(function(course) {
+          return <div>
+            {course.title}
+          </div>
+        })
+      }
     </div>
   );
 }
