e6ac6c59190d7d0cf21bb4ec80bc338c022f73a8
CraigStueber <craig@kurbmedia.com>
2020-08-09 09:05:10 -0400

    Learning

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const courses = [
   },
   {
     id:2,
-    title: "The fuck",
-    author: "Maximilian Schwarzmuller",
+    title: "The course name",
+    author: "J.R.R. Tolkien",
     hours_video: 40.5,
     number_of_lectures: 490,
     rating: 4.6,
@@ -23,8 +23,8 @@ const courses = [
   },
   {
     id:3,
-    title: "I like to eat ass",
-    author: "Maximilian Schwarzmuller",
+    title: "I need to fill in",
+    author: "R.A. Salvatore",
     hours_video: 40.5,
     number_of_lectures: 490,
     rating: 4.6,
@@ -41,26 +41,30 @@ function App() {
 return (
 
     <div>
-      <h1>List of shit</h1>
+      <h1>List of Courses</h1>
       <hr />
-      {
-        courses.map(function(course) {
-          return <div
-            key={course.id}>
-            <span>
-              <a href={course.url}><h4>{course.title}</h4></a>
-            </span>
-            <span>| by <strong>{course.author}</strong></span>
-            <span>| Video Hours: {course.hours_video}</span>
-            <span>| Number of lectures: {course.number_of_lectures}</span>
-            <span>| Rating: {course.rating}</span>
-            <br/>
-            <br/>
-          </div>
-        })
-      }
+      <CoursesList />
+      
     </div>
   );
+}
+
+function CoursesList(){
+return courses.map(function(course) {
+    return( <div
+      key={course.id}>
+      <span>
+        <a href={course.url}><h4>{course.title}</h4></a>
+      </span>
+      <span>| by <strong>{course.author}</strong></span>
+      <span>| Video Hours: {course.hours_video}</span>
+      <span>| Number of lectures: {course.number_of_lectures}</span>
+      <span>| Rating: {course.rating}</span>
+      <br/>
+      <br/>
+    </div>
+    );
+  });
 }
 
 export default App;
